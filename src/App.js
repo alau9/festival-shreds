ccffeb76bfb4d717450f8e5f65f1eafdb935be51
Anson <ansonlau39@gmail.com>
2021-12-24 20:21:52 -0800

    updated firebase util so it can save user info

--- src/App.js
+++ src/App.js
@@ -7,13 +7,48 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import Footer from './components/footer/footer.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+
 
 class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state={
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+        console.log(this.state)
+      }
+      
+      else{this.setState({currentUser: userAuth})}
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
   render(){
     return (
       <div >
-        <Header />
+        <Header currentUser={this.state.currentUser}/>
          <Routes>
            <Route exact path='/' element={<HomePage/>} />
            <Route exact path='/shop' element={<ShopPage/>} />
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,45 +1,64 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../firebase/firebase.utils";
+
 import { ReactComponent as Logo } from "../../assets/festival-shreds.svg";
 import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./header.styles.scss";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <div className="header-header">
-    <div className="left-side">Search</div>
-        <Link to="/" className="logo-container">
-          <Logo className="logo" />
-        </Link>
-        <div className="right-side">
-          <span onClick={signInWithGoogle}>Account</span> 
-          <span>Cart</span>
-        </div> 
+      <div className="left-side">Search</div>
+      <Link to="/" className="logo-container">
+        <Logo className="logo" />
+      </Link>
+      <div className="right-side">
+        {currentUser ? (
+          <div className="dropdown">
+            <span>Account</span>
+            <ul id="myDropdown" className="dropdown-content">
+              <li>My Orders</li>
+              <li>View Profile</li>
+              <li>My Wishlist</li>
+              <li>Edit Profile</li>
+              <li onClick={() => auth.signOut()}>Logout</li>
+            </ul>
+          </div>
+        ) : (
+          <div className="dropdown">
+            {" "}
+            <span onClick={signInWithGoogle}>Account</span>
+          </div>
+        )}
+
+        <span>Cart</span>
+      </div>
     </div>
-      <div className="options">
-        <Link className="option" to="/shop">
-          WOMENS
-        </Link>
-        <Link className="option" to="/shop">
-          ACCESSORIES
-        </Link>
-        <Link className="option" to="/shop">
-          MEN
-        </Link>
-        <Link className="option" to="/shop">
-          VISIONARY ART
-        </Link>
-        <Link className="option" to="/shop">
-          FREE SHIPPING
-        </Link>
-        <Link className="option" to="/shop">
-          BESTSELLERS
-        </Link>
-        <Link className="option" to="/shop">
-          BLOG
-        </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        WOMENS
+      </Link>
+      <Link className="option" to="/shop">
+        ACCESSORIES
+      </Link>
+      <Link className="option" to="/shop">
+        MEN
+      </Link>
+      <Link className="option" to="/shop">
+        VISIONARY ART
+      </Link>
+      <Link className="option" to="/shop">
+        FREE SHIPPING
+      </Link>
+      <Link className="option" to="/shop">
+        BESTSELLERS
+      </Link>
+      <Link className="option" to="/shop">
+        BLOG
+      </Link>
     </div>
   </div>
 );
